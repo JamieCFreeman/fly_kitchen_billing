cce79867e964a1a10c9fffcfc2d6be271a8e9be3
JamieCFreeman <jcf236@cornell.edu>
2024-10-14 16:20:17 -0500

    testing with api

--- billing_from_gsheet.py
+++ billing_from_gsheet.py
@@ -3,5 +3,17 @@
 # Purpose:
 # Fly kitchen billing is done in a google sheet- want to automatically process billing.
 
+# Sources:
+# Google doc API explanation:
+# https://docs.gspread.org/en/latest/oauth2.html#service-account
+
+cred_file = '/home/jamie/fly-kitchen-billing-e82a4be570b3.json'
+
+import gspread
 
 
+gc = gspread.service_account(filename=cred_file)
+
+sh = gc.open("Fly Kitchen Ordering (Responses)")
+
+print(sh.sheet1.get('Form Responses 1'))
